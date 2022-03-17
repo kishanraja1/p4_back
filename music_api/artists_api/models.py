c8df87f4232e7c960566280807dac25f9352d42a
Kishan Raja <kishan.raja96@gmail.com>
2022-03-17 12:34:37 -0400

    change schema

--- music_api/artists_api/models.py
+++ music_api/artists_api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Artist(models.Model):
     name = models.TextField()
     genre = models.TextField()
-    soloArtists = models.BooleanField()
+    # soloArtists = models.BooleanField()
     language = models.TextField()
     # albums = models.('artists_api.Album')
 
