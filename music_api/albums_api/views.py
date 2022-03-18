51297c5ad3d2de7d0ad586924eb04ffd5f51a752
Nathan Freeman <NathanLFreeman@gmail.com>
2022-03-18 16:10:50 -0400

    closer but can't access searchQuery when receiving POST from fe

--- music_api/albums_api/views.py
+++ music_api/albums_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
-from rest_framework import generics
+from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework import generics, request
 from .serializers import AlbumSerializer
 from .models import Album
 from albums_api import spotify_client
@@ -16,9 +17,13 @@ class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Album.objects.all().order_by('id')
     serializer_class = AlbumSerializer
 
+@ensure_csrf_cookie
 def spotify_get_album_info(request):
-    album_query = spotify_client.spotify.search('Some Nights','album')
+    print(request.POST)
+    search_query = request.POST.get('searchQuery')
+    print(search_query)
+    album_query = spotify_client.spotify.search(f'{search_query}','album')
     album_obj = spotify_client.spotify.convert_album_data(album_query)
-    print(album_obj)
+    # print(album_obj)
     # my_obj = {"test_field":"hello", "test_2":"just testing"}
     return JsonResponse(album_obj)
