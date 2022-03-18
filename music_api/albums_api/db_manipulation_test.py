4d4ec1b3b6b754d46d6371c07cc40f2da00236a7
Nathan Freeman <NathanLFreeman@gmail.com>
2022-03-17 20:33:54 -0400

    insert artist function written but not able to test until I pull artist model into backend

--- music_api/albums_api/db_manipulation_test.py
+++ music_api/albums_api/db_manipulation_test.py
@@ -3,7 +3,11 @@ from spotify_client import spotify
 
 sp_api_call = spotify.search('South Pacific','album')
 sp_converted_data = spotify.convert_album_data(sp_api_call)
-print(sp_converted_data)
+# print(sp_converted_data)
+
+ladygaga_api_call = spotify.search('Lady Gaga','artist')
+lg_converted_data = spotify.convert_artist_data(ladygaga_api_call)
+print(lg_converted_data)
 
 def insert_album(name, year):
   conn = None
@@ -37,7 +41,39 @@ def insert_album(name, year):
 
   return album_id
 
-print(insert_album(sp_converted_data['name'], sp_converted_data['year']))
+def insert_artist(name, genre, image):
+  conn = None
+  artist_id = None
+  try: 
+    # SET UP DB CONNECT WITH psycopg2
+    # creates connection to PostgreSQL server
+    conn = psycopg2.connect("dbname=music") 
+    print (conn)
+
+    # creates a cursor (used to execute SQL statements)
+    cur = conn.cursor()
+
+    # execute the INSERT statement
+    sql = '''INSERT INTO artists_api_artist(name, year) VALUES(%s, %s, %s) RETURNING id'''
+    cur.execute(sql, (name, genre, image,))
+
+    # get the generated id back
+    artist_id = cur.fetchone()[0]
+
+    # commit the changes to the db
+    conn.commit()
+
+    # close communication with the db
+    cur.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+  finally:
+    if conn is not None:
+      conn.close()
+
+  return artist_id
+
+# print(insert_album(sp_converted_data['name'], sp_converted_data['year']))
 
 
 
