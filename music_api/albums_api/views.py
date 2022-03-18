703a5dfc88c06e1c9a033568f1c7ee0e050ed615
Nathan Freeman <NathanLFreeman@gmail.com>
2022-03-18 18:38:03 -0400

    Can search for album by name, retrieve and see album info, and add that album to database

--- music_api/albums_api/views.py
+++ music_api/albums_api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
-from rest_framework import generics, request
-from .serializers import AlbumSerializer
-from .models import Album
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, request, status
+
+from .serializers import AlbumSerializer, QueryAlbumSerializer
+from .models import Album, QueryAlbum
 from albums_api import spotify_client
 
 
@@ -24,6 +27,22 @@ def spotify_get_album_info(request):
     print(search_query)
     album_query = spotify_client.spotify.search(f'{search_query}','album')
     album_obj = spotify_client.spotify.convert_album_data(album_query)
-    # print(album_obj)
+    print(album_obj)
     # my_obj = {"test_field":"hello", "test_2":"just testing"}
     return JsonResponse(album_obj)
+
+class QueryAlbumList(APIView):
+    def post(self, request):
+        # serializer = QueryAlbumSerializer(data=request.data)
+        # print(request.data)
+        search_query = request.data['search_query']
+        print(search_query)
+        album_query = spotify_client.spotify.search(f'{search_query}','album')
+        album_obj = spotify_client.spotify.convert_album_data(album_query)
+        print(album_obj)
+        return JsonResponse(album_obj)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response({"status":"success", "data": serializer.data}, status=status.HTTP_200_OK)
+        # else:
+        #     return Response({"status":"error", "data":serializer.errors}, )
